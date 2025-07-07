d022f056cf5582e11eef7448aa05c6898b7185a9
Daniel Rodriguez <danjrod@gmail.com>
2025-07-07 23:35:27 +0200

    Generic Literal and RefFootnote refactored to avoid call

--- bin/fixer/md/literal.py
+++ bin/fixer/md/literal.py
@@ -1,27 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8; py-indent-offset:4 -*-
 ###############################################################################
-from typing import Any
-
-from .. import TARGET
-
 # Catches generic literal blocks and delivers them as they are to avoid normalization
 # and line processing
 class GenericLiteral:
-    targets: int = -TARGET.AD2AD
-    block_begin: str = "```"  # matches lines starting with this
-    block_end: str = block_begin
+    block_begin_re: str = r"(?P<bticks>````?)"  # matches lines starting with this
+    block_end_rex: str = r"\g<bticks>"
     normalize: bool = False
-
-    def __call__(self, lines: list[str]) -> tuple[list[str], bool]:
-        return lines, False  # return a simple copy
+    lineproc: bool = False
 
 # This catches reference links and footnote links as a block and delivers them back
 # intact. That means that each line is not "normalized" because it is seen as a block.
 class GenericRefFootNote:
-    targets: int = -TARGET.AD2AD
-    block_begin_re: str = "\[\^?[^]]+\]:\s+.*"
+    block_begin_re: str = r"\[\^?[^]]+\]:\s+.*"
     normalize: bool = False
-
-    def __call__(self, lines: list[str]) -> list[str]:
-        return lines  # return a simple copy
+    block_oneline: bool = True
